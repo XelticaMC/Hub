73d5c1120ce2173cf4da24069d4bdfb065c24e7f
Xeltica <xeltica@gmail.com>
2021-02-10 22:40:50 +0900

    ナイトメアワールド対応

--- src/main/java/work/xeltica/craft/hub/Main.java
+++ src/main/java/work/xeltica/craft/hub/Main.java
@@ -3,9 +3,7 @@ package work.xeltica.craft.hub;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.UUID;
 import java.util.logging.Logger;
 
@@ -14,39 +12,32 @@ import org.bukkit.GameMode;
 import org.bukkit.GameRule;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.World;
-import org.bukkit.WorldCreator;
-import org.bukkit.WorldType;
 import org.bukkit.World.Environment;
+import org.bukkit.WorldCreator;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
-import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BookMeta;
-import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.chat.TextComponentSerializer;
-import work.xeltica.craft.hub.EmptyChunkGenerator;
 
 public class Main extends JavaPlugin implements Listener {
     @Override
@@ -87,6 +78,11 @@ public class Main extends JavaPlugin implements Listener {
                 player.sendMessage("hub が未生成");
                 return true;
             }
+            var isWarping = isWarpingMap.get(player.getUniqueId());
+            if (isWarping != null && isWarping) {
+                player.sendMessage("移動中です！");
+                return true;
+            }
             if (player.getWorld().getUID().equals(worldUuid)) {
                 player.sendMessage("既にロビーです！");
                 return true;
@@ -96,14 +92,18 @@ public class Main extends JavaPlugin implements Listener {
                 public void run() {
                     var world = server.getWorld(worldUuid);
                     var loc = world.getSpawnLocation();
-                    writePlayerConfig(player);
+                    var savePosition =
+                        !player.getWorld().getName().equalsIgnoreCase("nightmare");
+                    writePlayerConfig(player, savePosition);
                     players = YamlConfiguration.loadConfiguration(playersFile);
                     player.getInventory().clear();
                     player.setGameMode(GameMode.ADVENTURE);
                     player.teleport(loc, TeleportCause.PLUGIN);
+                    isWarpingMap.put(player.getUniqueId(), false);
                 }
             }, 20 * 5);
             player.sendMessage("5秒後にロビーに移動します...");
+            isWarpingMap.put(player.getUniqueId(), true);
             return true;
         }
 
@@ -145,13 +145,32 @@ public class Main extends JavaPlugin implements Listener {
             forceAll = !forceAll;
             sender.sendMessage("forceAll を" + forceAll + "にしました");
             return true;
+        } else if (args[0].equalsIgnoreCase("nmbed")) {
+            if (args.length != 4) {
+                sender.sendMessage("/hub nmbed <x> <y> <z>");
+                return true;
+            }
+            var x = Double.parseDouble(args[1]);
+            var y = Double.parseDouble(args[2]);
+            var z = Double.parseDouble(args[3]);
+            var loc = new Location(server.getWorld(worldUuid), x, y, z);
+            getConfig().set("nightmareBedLocation", loc);
+            saveConfig();
+            sender.sendMessage("悪夢ベッドの位置を設定しました。");
+            return true;
         }
         return false;
     }
 
-    private void writePlayerConfig(Player player) {
+    private void writePlayerConfig(Player player, boolean savesLocation) {
         var uid = player.getUniqueId().toString();
-        var section = players.createSection(uid);
+
+        var section = players.getConfigurationSection(uid);
+
+        if (section == null) {
+            players.createSection(uid);
+        }
+
         var inv = player.getInventory();
         var items = new ItemStack[inv.getSize()];
 
@@ -159,7 +178,10 @@ public class Main extends JavaPlugin implements Listener {
             items[i] = inv.getItem(i);
         }
         
-        section.set("location", player.getLocation());
+        if (savesLocation) {
+            section.set("location", player.getLocation());
+        }
+
         section.set("items", items);
 
         try {
@@ -217,8 +239,6 @@ public class Main extends JavaPlugin implements Listener {
         var player = e.getPlayer();
         if (player.getWorld().getUID().equals(worldUuid)) {
             e.setCancelled(true);
-            var inv = player.getInventory();
-            inv.clear();
             player.setGameMode(GameMode.SURVIVAL);
 
             var world = getServer().getWorld("world");
@@ -228,16 +248,8 @@ public class Main extends JavaPlugin implements Listener {
                 player.teleport(world.getSpawnLocation(), TeleportCause.PLUGIN);
                 return;
             }
-            logger.info(String.join(",", section.getKeys(false)));
-            var itemsResult = section.get("items");
-            if (itemsResult == null) {
-                player.sendMessage(ChatColor.RED + "インベントリ復元失敗。これはおそらくバグなので、管理者に報告してください。 Code: 1");
-            } else {
-                var items = (ArrayList<ItemStack>)itemsResult;
-                for (var i = 0; i < items.size(); i++) {
-                    inv.setItem(i, items.get(i));
-                }
-            }
+
+            restoreInventory(player);
 
             var locationResult = section.get("location");
             if (locationResult == null || !(locationResult instanceof Location)) {
@@ -265,9 +277,68 @@ public class Main extends JavaPlugin implements Listener {
         }
     }
 
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerInteractBlock(PlayerInteractEvent e) {
+        var p = e.getPlayer();
+        var w = p.getWorld();
+        if (e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        if (w.getUID().equals(worldUuid)) {
+            // 呪いのベッド
+            var bedLoc = e.getClickedBlock().getLocation();
+            var loc = getConfig().getLocation("nightmareBedLocation");
+            if (loc == null) return;
+            if (
+                bedLoc.getBlockX() != loc.getBlockX() ||
+                bedLoc.getBlockY() != loc.getBlockY() ||
+                bedLoc.getBlockZ() != loc.getBlockZ()
+            ) return;
+            e.setCancelled(true);
+            var nightmare = getServer().getWorld("nightmare");
+            nightmare.setDifficulty(Difficulty.HARD);
+            nightmare.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+            nightmare.setGameRule(GameRule.DO_WEATHER_CYCLE, false);
+            nightmare.setGameRule(GameRule.MOB_GRIEFING, false);
+            nightmare.setTime(18000);
+
+            restoreInventory(p);
+            p.teleport(nightmare.getSpawnLocation());
+            p.playSound(p.getLocation(), Sound.BLOCK_END_PORTAL_SPAWN, SoundCategory.PLAYERS, 1, 0.5f);
+            p.sendMessage("ここは怖い敵がうじゃうじゃいる" + ChatColor.RED + "ナイトメアワールド" + ChatColor.RESET + "。");
+            p.sendMessage("手に入れたアイテムは持ち帰れます。");
+            p.sendMessage("帰るときは、" + ChatColor.GREEN + "/hub " + ChatColor.RESET + "コマンドを実行してください。");
+        }
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent e) {
+        if (e.getPlayer().getWorld().getName().equalsIgnoreCase("nightmare")) {
+            // 悪夢から目覚める
+            writePlayerConfig(e.getPlayer(), false);
+            players = YamlConfiguration.loadConfiguration(playersFile);
+            var lobby = getServer().getWorld(worldUuid).getSpawnLocation();
+            e.setRespawnLocation(lobby);
+        }
+    }
+
+    private void restoreInventory(Player player) {
+        ConfigurationSection section = players.getConfigurationSection(player.getUniqueId().toString());
+        var inv = player.getInventory();
+        inv.clear();
+        var itemsResult = section.get("items");
+        if (itemsResult == null) {
+            player.sendMessage(ChatColor.RED + "インベントリ復元失敗。これはおそらくバグなので、管理者に報告してください。 Code: 1");
+        } else {
+            var items = (ArrayList<ItemStack>) itemsResult;
+            for (var i = 0; i < items.size(); i++) {
+                inv.setItem(i, items.get(i));
+            }
+        }
+    }
+
     private Logger logger;
     private File playersFile;
     private YamlConfiguration players;
     private UUID worldUuid;
     private boolean forceAll;
+    private HashMap<UUID, Boolean> isWarpingMap = new HashMap<>();
 }
